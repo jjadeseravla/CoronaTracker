661a01093f18ca8ff2f4b0442eb59d9d91e88084
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-03 13:02:10 +0100

    Added spring annotations to run the app, which should GET all the info from the URL

--- src/main/java/io/javabrains/coronatracker/services/CoronaVirusDataService.java
+++ src/main/java/io/javabrains/coronatracker/services/CoronaVirusDataService.java
@@ -1,20 +1,35 @@
 package io.javabrains.coronatracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+@Service
 public class CoronaVirusDataService {
-
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     //make HTTP Call to URL
-    public void fetchVirusData() {
+    @PostConstruct//this annotation means execute this when the app starts
+    //After you've constructed this instance of the @Service, execute the fetchVirusData() method
+    public void fetchVirusData() throws IOException, InterruptedException {
+
         HttpClient client = HttpClient.newHttpClient();
+
+        //create a request using the builder pattern
         HttpRequest request = HttpRequest.newBuilder()
                                          .uri(URI.create(VIRUS_DATA_URL)) //says where you need to access
                                          .build();
+
+        //second argument in send() is the body handler where we are taking the body and returning it as a string
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(httpResponse.body());
     }
 
 }
