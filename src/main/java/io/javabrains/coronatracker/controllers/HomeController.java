30ca584067d9a90fdad40dd1bdb61d919344666c
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-08 11:38:28 +0100

    Set an attribute in the controller and looked it up on the template
    so the localhost:1212 says "TEST"

--- src/main/java/io/javabrains/coronatracker/controllers/HomeController.java
+++ src/main/java/io/javabrains/coronatracker/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package io.javabrains.coronatracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller //renders HTML UI...its NOT a rest controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("testName", "TEST");
         return "home"; //returning a name which points to the template
         //eg if you return "ho" it returns what is inside templates/ho.html
     }
